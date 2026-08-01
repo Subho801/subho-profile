2a78bc7cd5e91d59cc18d35e90de4a570da34179
Arthur Morgan <lofibeatz95@gmail.com>
2026-08-01 23:15:19 +0530

    Create route.ts

---
+++ app/api/rog/route.ts
@@ -0,0 +1,21 @@
+export async function GET() {
+  const res = await fetch(
+    "https://cdn.jsdelivr.net/gh/Subho801/rog-rewards-notifier@main/website/rog.json",
+    {
+      next: {
+        revalidate: 300,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    return Response.json(
+      { error: "Failed to fetch ROG data" },
+      { status: 500 }
+    );
+  }
+
+  const data = await res.json();
+
+  return Response.json(data);
+}
