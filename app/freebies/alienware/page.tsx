59cb3a84424567b73bf776797281996daa0de3a2
Arthur Morgan <lofibeatz95@gmail.com>
2026-08-02 17:37:41 +0530

    Fix formatting in alienware page component

--- app/freebies/alienware/page.tsx
+++ app/freebies/alienware/page.tsx
@@ -269,7 +269,7 @@ export default function AlienwarePage() {
 
                         
 
-                      </div>
+                      
 
                       <a
                         href={giveaway.url}
